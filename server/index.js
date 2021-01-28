5346c22ba534a4f938f9d8f2a4653e5aaf0cf02d
Christie Jensen <christiejensen@Christies-MacBook-Air.local>
2021-01-28 14:07:30 -0700

    server built, postman tests passed

--- server/index.js
+++ server/index.js
@@ -1,10 +1,18 @@
 const express = require('express')
+const mc = require('./controllers/messages_controller')
 
 const app = express()
 
 app.use(express.json())
+app.use(express.static(__dirname + '/../public/build'))
 
-port = 3001
+app.get('/api/messages', mc.read)
+app.post('/api/messages', mc.create)
+app.put('/api/messages/:id', mc.update)
+app.delete('/api/messages/:id', mc.delete)
 
-app.listen(port, () => `Server is running on port ${port}`)
+const port = 3001
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
 
